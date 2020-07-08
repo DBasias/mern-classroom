7771c84194920b7d8917505eb02964259702d5a6
DBasias <dbasiadimas@hotmail.com>
2020-07-08 19:44:25 +0300

    created enrollment model, routes and backend api methods to find and create enrollments

---
+++ server/routes/enrollment.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import authCtrl from "../controllers/auth.controller";
+import courseCtrl from "../controllers/course.controller";
+import enrollmentCtrl from "../controllers/enrollment.controller";
+
+const router = express.Router();
+
+router
+  .route("/api/enrollment/new/:courseId")
+  .post(
+    authCtrl.requireSignin,
+    enrollmentCtrl.findEnrollment,
+    enrollmentCtrl.create
+  );
+
+router.param("courseId", courseCtrl.courseByID);
+
+export default router;
